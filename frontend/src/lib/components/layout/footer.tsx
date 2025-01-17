91c50f818860e8f7046f4a28bc9d510e4c78730a
BitFancy <bitbanana717@gmail.com>
2025-01-18 03:01:19 +0900

    update: homepage and footer responsive

--- frontend/src/lib/components/layout/footer.tsx
+++ frontend/src/lib/components/layout/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     return (
         <footer className="bg-[#1d2833] dark:bg-gray-800 pb-8 pt-10">
             <div className="container mx-auto px-4">
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 mx-6 md:grid-cols-4 gap-8">
                     <div>
                         <div
                             className="inline"
